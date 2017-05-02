142d49812bce572de2b0cd18caa7949a54c48aa5
cassandragoodby <cassandra.goodby@colorado.edu>
2017-05-01 21:52:29 -0600

    initial commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(document).scroll(function () {
+var y = $(this).scrollTop();
+if (y > 150) {
+    $('#img2').fadeIn();
+}
+else {$('#img2').fadeOut('fast')};
+
+if (y > 300) {
+    $('#img3').fadeIn();
+}
+else {$('#img3').fadeOut('fast')};
+
+if (y > 450) {
+    $('#img4').fadeIn();
+}
+else {$('#img4').fadeOut('fast')};
+});
